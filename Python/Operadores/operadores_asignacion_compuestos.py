6bec081d211d0e96bf2f7b1609f7c242c5473abc
Andrés Camilo <135870143+camilov3@users.noreply.github.com>
2025-02-22 19:21:50 -0500

    Avance de hoy

---
+++ Python/Operadores/operadores_asignacion_compuestos.py
@@ -0,0 +1,21 @@
+print("*** Operadores de asignación compuestos ***")
+
+a, b = 10,15
+
+print(f"\nValor inicial de a: {a}, valor inicial  de b: {b}")
+
+# Operador compuesto de suma +=
+a += b # Esto representa a = a + b
+print(f"\nValor actual de a: {a}")
+
+# Operador compuesto de resta -=
+a -= b # Esto representa a = a - b
+print(f"\nValor actual de a: {a}")
+
+# Operador compuesto de multiplicación *=
+a *= b # a = a * b
+print(f"\nValor actual de a: {a}")
+
+# Operador compuesto de división /=
+a //= b
+print(f"\n Valor actual de a: {a}")
