054a3e22471681aee3be5c2dc21db6dd3d6d6ddc
Andrés Camilo <135870143+camilov3@users.noreply.github.com>
2025-03-11 10:34:33 -0500

    empezando ciclos

---
+++ Python/Ciclos/ciclo_while.py
@@ -0,0 +1,9 @@
+print('*** Ciclo While ***')
+
+# Imprimir los valores del 1 al 5
+
+contador = 1
+while contador <= 5:
+    print(contador, end=' ') # Le estoy dando un formato de impresión por cada línea que termina de imprimirse, en este caso, solo deja un espacio
+    # print(contador, end=' - ') # En este caso me dejaría un espacio seguido de un guión
+    contador += 1 # contador = contador + 1
